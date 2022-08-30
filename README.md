e2d470ac5a83c66eff273de3a2605734965668cb
Adekabang <iban.ninja@gmail.com>
2022-08-30 21:27:32 +0700

    fix typo

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ```sh
 apt install git vim -y
 git clone https://github.com/Adekabang/RISS-pve.git
-cd RISS-vpe
+cd RISS-pve
 
 cd theme-xxx
 ./changelogo.sh
--- theme-dark-orange/changelogo.sh
+++ theme-dark-orange/changelogo.sh
@@ -1,5 +1,4 @@
-mv /usr/share/pve-manager/images/proxmox_logo.png /usr/share/pve-manager/images/proxmox_logo.png.bak
-mv RISS-virt-Manager.png /usr/share/pve-manager/images/proxmox_logo.png
-cd /usr/share/pve-manager/images/
-mv /usr/share/pve-manager/images/logo-128.png /usr/share/pve-manager/images/logo-128.png.bak
-mv favicon.png /usr/share/pve-manager/images/logo-128.png
+cp /usr/share/pve-manager/images/proxmox_logo.png /usr/share/pve-manager/images/proxmox_logo.png.bak
+cp RISS-virt-Manager.png /usr/share/pve-manager/images/proxmox_logo.png
+cp /usr/share/pve-manager/images/logo-128.png /usr/share/pve-manager/images/logo-128.png.bak
+cp favicon.png /usr/share/pve-manager/images/logo-128.png
