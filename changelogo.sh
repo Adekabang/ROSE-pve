7113110599624a02e6046a24ef6eb1f6f3a0df6c
Adekabang <iban.ninja@gmail.com>
2022-08-30 20:49:43 +0700

    initial commit

---
+++ changelogo.sh
@@ -0,0 +1,5 @@
+mv /usr/share/pve-manager/images/proxmox_logo.png /usr/share/pve-manager/images/proxmox_logo.png.bak
+mv RISS-virt-Manager.png /usr/share/pve-manager/images/proxmox_logo.png
+cd /usr/share/pve-manager/images/
+mv /usr/share/pve-manager/images/logo-128.png /usr/share/pve-manager/images/logo-128.png.bak
+mv favicon.png /usr/share/pve-manager/images/logo-128.png
