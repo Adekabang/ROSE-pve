a33d37022732385e6a3e0f584331021cad9acaeb
Adekabang <iban.ninja@gmail.com>
2024-07-09 19:37:23 +0700

    test:modify el timezone

--- create-template-staging.sh
+++ create-template-staging.sh
@@ -35,9 +35,9 @@ customize_image() {
             virt-customize -a "$file_name" --run-command 'sed -i "s/^#PermitRootLogin prohibit-password/PermitRootLogin yes/" /etc/ssh/sshd_config'
             ;;
         "centos"|"rocky"|"alma")
-            virt-customize -a "$file_name" --run-command "dnf install -y vim qemu-guest-agent"
-            virt-customize -a "$file_name" --run-command "systemctl enable qemu-guest-agent"
-            virt-customize -a "$file_name" --timezone "$timezone"
+            virt-customize -a "$file_name" --run-command "dnf install -y vim"
+            virt-customize -a "$file_name" --run-command "sudo timedatectl set-timezone $timezone"
+            # virt-customize -a "$file_name" --timezone "$timezone"
             ### Enable SSH access
             virt-customize -a "$file_name" --run-command 'sed -i -e "s/^#Port 22/Port 22/" -e "s/^#AddressFamily any/AddressFamily any/" -e "s/^#ListenAddress 0.0.0.0/ListenAddress 0.0.0.0/" -e "s/^#ListenAddress ::/ListenAddress ::/" /etc/ssh/sshd_config'
             ### Allow PasswordAuthentication
