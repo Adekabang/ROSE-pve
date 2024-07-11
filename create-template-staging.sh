fc8d41087f6b8fc2a7363a9676963872d991e657
Adekabang <iban.ninja@gmail.com>
2024-07-11 20:12:36 +0700

    el install

--- create-template-staging.sh
+++ create-template-staging.sh
@@ -38,7 +38,8 @@ customize_image() {
         "centos"|"rocky"|"alma")
             virt-customize -a "$file_name" --install wget
             virt-customize -a "$file_name" --selinux-relabel --timezone $timezone
-            virt-customize -a "$file_name" --run-command "echo 'PasswordAuthentication yes' >> /etc/ssh/sshd_config.d/01-allow-password-auth"
+            virt-customize -a "$file_name" --run-command "touch /etc/ssh/sshd_config.d/01-allow-password-auth.conf "
+            virt-customize -a "$file_name" --run-command "echo 'PasswordAuthentication yes' >> /etc/ssh/sshd_config.d/01-allow-password-auth.conf"
             ### Enable SSH access
             virt-customize -a "$file_name" --run-command 'sed -i -e "s/^#Port 22/Port 22/" -e "s/^#AddressFamily any/AddressFamily any/" -e "s/^#ListenAddress 0.0.0.0/ListenAddress 0.0.0.0/" -e "s/^#ListenAddress ::/ListenAddress ::/" /etc/ssh/sshd_config'
             ### Allow PasswordAuthentication
